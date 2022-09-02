1dead9985046a79de763a4da9cf7a5eb422eb280
espinosasteban <esespinosap@unal.edu.co>
2022-09-02 16:14:52 -0500

    arreglos

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,32 +6,43 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = 12
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
+
+    def listadoAsignaturas(self, **kwargs):
+        if self._asignaturas is None:
+            self._asignaturas = []
+
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+        if self.listadoAlumnos is None:
+            self.listadoAlumnos = []
+            if lista is None:
+                self.listadoAlumnos = [alumno]
+            else:
+                for elemento in lista:
+                    self.listadoAlumnos.append(elemento)
+                self.listadoAlumnos.append(alumno)
         else:
-            self.listadoAlumnos = [alumno]
+            if lista is None:
+                self.listadoAlumnos = [alumno]
+            else:
+                for elemento in lista:
+                    self.listadoAlumnos.append(elemento)
+                self.listadoAlumnos.append(alumno)
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+
+
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
